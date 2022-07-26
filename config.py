ee0dec4c59b64fbd266fc2af048d66099717f215
ahmedalhajjj <110001296+ahmedalhajjj@users.noreply.github.com>
2022-07-26 16:21:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ GROUP_SUPPORT = getenv("GROUP_SUPPORT", "dragon963")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "uxxx_u")
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL")
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $ ").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "").split())
 OWNER_ID = list(map(int, getenv("OWNER_ID").split()))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
